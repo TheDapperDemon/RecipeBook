43100b086177c0854a0d3a9c10c928f4b3b8bd99
TheDapperDemon <83481628+TheDapperDemon@users.noreply.github.com>
2022-07-25 13:00:19 -0600

    Created Test Recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe (
+      'Test Recipe', 
+      'This is a test', 
+      'https://media.istockphoto.com/vectors/healthy-recipes-weight-loss-flat-design-icon-vector-id1078219340?k=20&m=1078219340&s=612x612&w=0&h=dn6FllWFfQ1QVwuMknrQ9EUXDe3PUM0d8kjgFZo4Prk='
+      )
+  ];
+
 
   constructor() { }
 
